b41087c7e98c522b397f055130bc944d8cd5fcad
muukii <muukii.muukii@gmail.com>
2014-10-19 20:37:58 +0900

    Initial Commit

---
+++ ColorGen/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ColorGen
+//
+//  Created by Muukii on 10/19/14.
+//  Copyright (c) 2014 Muukii. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
